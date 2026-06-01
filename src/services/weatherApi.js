fbb60a43b7f88b15626844201d4bf1906127e8c1
Prem Thakur <pkpremchand786@gmail.com>
2026-06-02 02:56:49 +0500

    updated project

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,12 +1,11 @@
-const API_KEY = import.meta.env.VITE_WEATHER_API_KEY || "4d8fb5b93d4af21d66a2948710284366";
-
 export const getWeather = async (city) => {
-  const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&units=metric&appid=${API_KEY}`
+  const response = await fetch(
+    `https://weather-backend-gamma.vercel.app/weather?city=${city}`
   );
-  if (!res.ok) {
-    const err = await res.json();
-    throw new Error(err.message === "city not found" ? "City not found. Please try again." : "Something went wrong.");
+
+  if (!response.ok) {
+    throw new Error("City not found");
   }
-  return res.json();
+
+  return response.json();
 };
